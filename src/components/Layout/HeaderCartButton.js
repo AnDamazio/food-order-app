5f3c1a293327d3bd31d7cace4572e55c91b71cc8
AnDamazio <damazioandre@hotmail.com>
2022-08-18 15:32:00 -0300

    Add functionally: add item to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,25 @@
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
+import { useContext } from "react";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    console.log(curNumber, item.amount);
+    return curNumber + item.amount;
+  }, 0);
   return (
-    <button className={classes.button}>
-      <span className={classes.icon}>
-        <CartIcon />
-      </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
-    </button>
+    <CartContext.Provider value={cartCtx}>
+      <button className={classes.button} onClick={props.onClick}>
+        <span className={classes.icon}>
+          <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
+      </button>
+    </CartContext.Provider>
   );
 };
 
